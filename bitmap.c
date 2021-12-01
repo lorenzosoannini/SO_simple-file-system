b682342b8ac41688572a17568285107101ef75ee
sanjohngiangino <marcelo.1850518@studenti.uniroma1.com>
2021-12-01 17:33:31 +0100

    creao file bitmap.c

---
+++ bitmap.c
@@ -0,0 +1,21 @@
+#include "bitmap.h"
+
+// JM implemento funzione BitMapEntryKey BitMap_blockToIndex(int num)
+//  converto un blocco indice a un indice nell'array
+// e un char che indica l'offset del bit all'interno dell'array
+
+BitMapEntryKey BitMap_blockToIndex(int num) {
+	BitMapEntryKey obstructionum;
+	
+	//JM la divisione intera restituisce l'indice dell array
+	obstructionum.entry_num = num / 8 ;
+	
+	//JM il resto della divisione indica lo spiazzamento all'interno del blocco restituito dalla divisione precedente
+	obstructionum.bit_num = num % 8;
+	
+	
+	return obstructionum;
+	
+	
+}
+
