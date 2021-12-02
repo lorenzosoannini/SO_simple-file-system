65c8b5b26af3a709f457e4408a3d576ce4a8b4d1
sanjohngiangino <marcelo.1850518@studenti.uniroma1.com>
2021-12-02 14:15:15 +0100

    JM implemento funzione DiskDriver_init su disk_driver.c con errore di bus ( core dump creato)??

--- disk_driver.c
+++ disk_driver.c
@@ -6,7 +6,12 @@
 // se il file è nuovo compila un nuovo disk header,
 // lo riempe  nella bitmap di dimensione appropiata con tutti 0 
 // ad indicare gli spazi vuoti
+
+
 void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
+	
+	//variabile f dove memorizzo il file
+	int f;
 	
 	//JM se il file esiste
 	if(!access(filename, F_OK)) {
@@ -15,6 +20,10 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
 			printf("C'è stato un errore nell'apertura del file. Il programma è stato bloccato.");
 			return;
 		}
+		
+		//assegno i valori int fd e DiskHeader header 
+		disk->fd = f;
+		disk->header = (DiskHeader*) mmap(0, sizeof(DiskHeader) + num_blocks + (num_blocks*BLOCK_SIZE), PROT_READ | PROT_WRITE, MAP_SHARED, f, 0);
 		
 		
 	}else{
@@ -26,7 +35,22 @@ void DiskDriver_init(DiskDriver* disk, const char* filename, int num_blocks){
 		}
 		
 		
+		disk->fd=f;
+		//creo un nuovo DiskHeader e lo riempo nella bitmap n
+		disk->header = (DiskHeader*) mmap(0, sizeof(DiskHeader) + num_blocks + (num_blocks*BLOCK_SIZE), PROT_READ | PROT_WRITE, MAP_SHARED, f, 0);
+		
+		
+		disk->header->num_blocks = num_blocks;
+		disk->header->bitmap_blocks = count_blocks(num_blocks);
+		disk->header->bitmap_entries = num_blocks;
+		disk->header->free_blocks = num_blocks;
 	}
+	lseek(f, 0, SEEK_SET);
+	// Memorizzo in bitmap_data il puntatore alla mmap saltando lo spazio dedicato a DiskHeader
+	disk->bitmap_data = (char *) disk->header + sizeof(DiskHeader);
 
+	// Calcolo il primo blocco libero dopo aver assegnato il valore alle entries
+	disk->header->first_free_block = DiskDriver_getFreeBlock(disk,0);
+	
 	return;
 }
